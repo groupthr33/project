a7bcf0de4b75ba62f83ac09c951438e5a8c259a2
Brian Marsolek <marsole4@uwm.edu>
2019-04-21 18:06:34 -0500

    US 168
    Created cr_course.html and views/cr_course.py

---
+++ app/views/cr_course.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.views import View
+from app.services.auth_service import AuthService
+from app.services.account_service import AccountService
+from app.services.course_service import CourseService
+
+auth_service = AuthService()
+account_service = AccountService()
+course_service = CourseService()
+
+
+class CreateCourse(View):
+    def get(self, request):
+        return render(request, 'main/cr_course.html')
+
+    def post(self, request):
+        coures_id = request.POST['course_id']
+        section = request.POST['section']
+        name = request.POST['name']
+        schedule = request.POST['schedule']
+
+        cr_course_response = course_service.create_course(coures_id, section, name, schedule)
+
+        context = {'message': cr_course_response}
+
+        return render(request, 'main/cr_course.html', context)
