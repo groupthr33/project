00c4becdc234f0d553dd94d18cf7ba168f780cce
Bee-Yang <yang737@uwm.edu>
2019-04-24 15:19:01 -0500

    bee-fixed-context-processor-tests

--- app/unit_tests/middleware/test_context_processor.py
+++ app/unit_tests/middleware/test_context_processor.py
@@ -24,12 +24,71 @@ class TestContextProcessor(TestCase):
             Command("set_password", "Change Password", 0xF, True),
             Command("update_contact", "Update Contact Info", 0xF, True),
             Command("view_contact_info", "Users", 0xF, True),
-            Command("cr_course", "Create Course", 0xC),
-            Command("cr_lab", "Create Lab", 0xC),
-            Command("assign_ta_lab", "Assign TA to Lab", 0xA),
+            Command("cr_course", "Create Course", 0xC, True),
             Command("my_courses", "My Courses", 0x2, True),
-            Command("view_lab_details", "View Lab Details", 0xC),
             Command("view_courses", "Courses", 0xC, True),
+            Command("my_courses_ta", "My Courses Ta", 0x1, True)
+        ], 'username': 'theuser',
+            'active': 'test_path'}
+
+        actual_response = ctxp.commands(request)
+        self.assertEqual(expected_response, actual_response)
+
+    def test_allowed_commands_supervisor(self):
+        self.account.roles = 0x8
+        self.account.save()
+
+        request = HttpRequest()
+        request.session = {'username': 'theuser'}
+        request.path = "/test_path/"
+
+        expected_response = {'commands': [
+            Command("cr_account", "Create Account", 0xC, True),
+            Command("set_password", "Change Password", 0xF, True),
+            Command("update_contact", "Update Contact Info", 0xF, True),
+            Command("view_contact_info", "Users", 0xF, True),
+            Command("cr_course", "Create Course", 0xC, True),
+            Command("view_courses", "Courses", 0xC, True),
+        ], 'username': 'theuser',
+            'active': 'test_path'}
+
+        actual_response = ctxp.commands(request)
+        self.assertEqual(expected_response, actual_response)
+
+    def test_allowed_commands_admin(self):
+        self.account.roles = 0x4
+        self.account.save()
+
+        request = HttpRequest()
+        request.session = {'username': 'theuser'}
+        request.path = "/test_path/"
+
+        expected_response = {'commands': [
+            Command("cr_account", "Create Account", 0xC, True),
+            Command("set_password", "Change Password", 0xF, True),
+            Command("update_contact", "Update Contact Info", 0xF, True),
+            Command("view_contact_info", "Users", 0xF, True),
+            Command("cr_course", "Create Course", 0xC, True),
+            Command("view_courses", "Courses", 0xC, True),
+        ], 'username': 'theuser',
+            'active': 'test_path'}
+
+        actual_response = ctxp.commands(request)
+        self.assertEqual(expected_response, actual_response)
+
+    def test_allowed_commands_instructor(self):
+        self.account.roles = 0x2
+        self.account.save()
+
+        request = HttpRequest()
+        request.session = {'username': 'theuser'}
+        request.path = "/test_path/"
+
+        expected_response = {'commands': [
+            Command("set_password", "Change Password", 0xF, True),
+            Command("update_contact", "Update Contact Info", 0xF, True),
+            Command("view_contact_info", "Users", 0xF, True),
+            Command("my_courses", "My Courses", 0x2, True),
         ], 'username': 'theuser',
             'active': 'test_path'}
 
@@ -48,6 +107,7 @@ class TestContextProcessor(TestCase):
             Command("set_password", "Change Password", 0xF, True),
             Command("update_contact", "Update Contact Info", 0xF, True),
             Command("view_contact_info", "Users", 0xF, True),
+            Command("my_courses_ta", "My Courses Ta", 0x1, True)
         ], 'username': 'theuser',
             'active': 'test_path'}
 
