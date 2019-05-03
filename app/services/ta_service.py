850a86bfc3d25e25c79285e7b45c3d5f89c9c02b
Brian Marsolek <marsole4@uwm.edu>
2019-05-03 11:35:20 -0500

    Unassign TA from Course and Lab
    initial work

--- app/services/ta_service.py
+++ app/services/ta_service.py
@@ -97,3 +97,55 @@ class TaService:
 
         return result_string + \
             f"{ta_course_rel.remaining_sections} section(s) remaining for {ta_user_name}."
+
+    def unassign_ta_to_course(self, account, course_id, section):
+        courses = Course.objects.filter(course_id__iexact=course_id, section__iexact=section)
+        if courses.count() == 0:
+            return f"Course with ID {course_id}-{section} does not exist."
+        course = courses.first()
+
+        users = Account.objects.filter(username__iexact=account)
+        if users.count() == 0:
+            return f"{account} dne."
+        ta = users.first()
+
+        if (ta.roles & 0x1) == 0:
+            return f"{ta.username} does not have the ta role."
+
+        ta_course = TaCourse.objects.filter(course=course, assigned_ta=ta)
+        if ta_course.count() == 0:
+            return f"{account} is not assigned to {course_id}-{section}."
+
+        for lab in ta_course:
+            lab.delete()
+
+        return f"{account} has been unassigned from {course_id}-{section}."
+
+    def unassign_ta_to_lab(self, ta_user_name, course_id, course_section, lab_sections, requester):
+        tas = Account.objects.filter(username__iexact=ta_user_name)
+        if tas.count() == 0:
+            return f"TA with user_name {ta_user_name} does not exist."
+        ta = tas.first()
+
+        if ta.roles & 0x1 == 0:
+            return f"{ta_user_name} does not have the ta role."
+
+        courses = Course.objects.filter(course_id__iexact=course_id, section__iexact=course_section)
+        if courses.count() == 0:
+            return f"Course with ID {course_id}-{course_section} does not exist."
+        course = courses.first()
+
+        requesters = Account.objects.filter(username__iexact=requester)
+        if requesters.count() == 0:
+            return "Requester does not exist."
+        requester = requesters.first()
+
+        if requester.roles & 0x8 == 0 \
+                and (course.instructor is None or course.instructor.username != requester.username):
+            return f"{requester.username} is not the instructor for {course_id}-{course_section}."
+
+        ta_course_rels = TaCourse.objects.filter(course=course, assigned_ta=ta)
+        if ta_course_rels.count() == 0:
+            return f"{ta_user_name} is not assigned to course {course_id}-{course_section}."
+
+
--- app/web_acceptance_tests/test_unassign_ta_course.py
+++ app/web_acceptance_tests/test_unassign_ta_course.py
@@ -1,15 +1,20 @@
 from django.test import TestCase
 from django.test import Client
 from app.models.account import Account
+from app.models.ta_course import TaCourse
+from app.models.course import Course
 
 
 class TestUnassignTaCourse(TestCase):
 
     def setUp(self):
         self.user = Account.objects.create(username='super_visor', password='p', name='n', is_logged_in=True, roles=0x8)
-        self.account = Account.objects.create(username='acc', password='p', name='n', is_logged_in=False, roles=0x1)
+        self.ta = Account.objects.create(username='test_ta', password='p', name='n', is_logged_in=False, roles=0x1)
 
         self.client = Client()
+        self.session = self.client.session
+        self.session['username'] = 'theuser'
+        self.session.save()
 
     def test_unassign_ta_course(self):
         self.fail()
